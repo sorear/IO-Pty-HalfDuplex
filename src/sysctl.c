86e3e25880f038882be0678ade03851f7bf48e0e
Stefan O'Rear <sorear@sorear.sorear>
2009-06-13 09:19:22 +0000

    Add xsub bits for the SysctlPoll backend

---
+++ src/sysctl.c
@@ -0,0 +1,25 @@
+#include <sys/sysctl.h>
+#include <sys/user.h>
+#include <string.h>
+
+int
+iphd_sysctl_is_waiting(int pid)
+{
+    struct kinfo_proc kip;
+    int addr[4];
+    int err;
+
+    addr[0] = CTL_KERN;
+    addr[1] = KERN_PROC;
+    addr[2] = KERN_PROC_PID;
+    addr[3] = pid;
+
+    err = sysctl(addr, 4, NULL, NULL, &kip, sizeof(kip));
+
+    if (err < 0) {
+        /* can happen due to races, so ignore XXX */
+        return 0;
+    }
+
+    return kip.ki_stat == 'S' && !strcmp(kip.ki_wmesg, "ttyin");
+}
